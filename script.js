1aa3cbd57b60ddef4d5b4d3177cff0394df96196
tdrayson <33532995+tdrayson@users.noreply.github.com>
2021-09-09 22:24:15 +0100

    Added current day as begining of week

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ var cal, resizeThrottled;
 var useDetailPopup = true;
 var datePicker, selectedCalendar;
 
+var currentDate = new Date();
+var dateIndex = currentDate.getDay()
+
 var calendar = new Calendar('#calendar', {
     defaultView: 'week',
     taskView: false,
     useDetailPopup: useDetailPopup,
+    week: {
+        startDayOfWeek: dateIndex, // monday
+    },
     isReadOnly: true,
-    startDayOfWeek: 1, // monday
+    
     template: {
         monthDayname: function(dayname) {
           return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
@@ -31,12 +37,13 @@ fetch(scheduleAPI).then(function (response) {
     data.forEach(streamer => {
         //console.log(streamer);
 
+        schedule = streamer.segments;
+        if(schedule){
         displayName = streamer.display_name;
         loginName = streamer.login;
         console.log(displayName);
         description = streamer.description;
         profileImage = '<img class="profile-image" src="' + streamer.profile_image_url + '">';
-        schedule = streamer.segments;
         var randomColour = '#' + Math.floor(Math.random()*16777215).toString(16);
         //console.log(schedule);
         if(schedule){
@@ -48,7 +55,6 @@ fetch(scheduleAPI).then(function (response) {
                 category = streamEvent.category;
                 if(category){categoryName = category.name};
                 let event = {};
-                //event['calendarId'] = 'Prashant Yadav';
                 event['title'] = title + ' - ' + displayName;
                 event['body'] = profileImage + '<br>' +  description + '<br><a class="button" target="_blank" href="https://twitch.tv/'+ loginName + '>Watch stream</a>';
                 event['category'] = 'time';
@@ -62,29 +68,11 @@ fetch(scheduleAPI).then(function (response) {
 
             }
         }
-    });
+    }});
 
-//     $.each(data, function(i, models) {
-//         model = models.model;
-//        size = models.size;
-//        console.log(models.model);
-//        console.log(models.size);
-//        carModel.append("<option value="+size+">" + model + "</option>");
-//    });
-    console.log(events);
+    //console.log(events);
     calendar.createSchedules(events);
-    // calendar.createSchedules([
-    //     {
-    //         calendarId: '1',
-    //         title: '⚡️ THIS IS WEB DEV ⚡️ - whitep4nth3r',
-    //         body: '✨⚡️ I help developers build stuff, learn things, and love what they do • DevRel @contentful • @Twitch partner • @Microsoft MVP • unbreak.tech founder • she/her ⚡️✨<br><a class="button" target="_blank" href="https://twitch.tv/whitep4nth3r">Watch stream</a>',
-    //         category: 'time',
-    //         start: '2021-09-10T18:00:00Z',
-    //         end: '2021-09-10T21:00:00Z',
-    //         isReadOnly: true,
-    //     },
-    // ]);
-    //console.log(data);
+
 }).catch(function (err) {
     // There was an error
     console.warn('Something went wrong.', err);
